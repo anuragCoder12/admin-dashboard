e81b2797141898ad54b9afb66c18f34d2d9e1777
anuragCoder12 <anuragpalliyali12@gmail.com>
2025-02-23 11:28:40 +0530

    commit

--- src/data/Endpoints/category.js
+++ src/data/Endpoints/category.js
@@ -4,4 +4,5 @@ export const CategoryApi = {
     getCat: (data) => get(`category`, { params: data }),
     postCat: (data) => post(`category/create`, data),
     deleteCat: (id) => destroy(`category/${id}`),
+    
 }
